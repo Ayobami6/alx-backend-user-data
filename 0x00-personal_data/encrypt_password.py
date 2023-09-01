c7512543032d2d699a4337196dbc986bef57b698
Ayobami <ayobamidele006@gmail.com>
2023-09-01 08:42:22 +0100

    implement password encryption

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python3
+""" encrypts password """
+
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> str:
+    """ Hash password function
+
+    Args:
+        password (str): password to encrypt
+
+    Returns:
+        str: encrypted password
+    """
+    # encode password
+    encoded_pssw = password.encode()
+    encrypted = hashpw(encoded_pssw, bcrypt.gensalt())
+    return encrypted
+
+
+def is_valid(hashed_pssw: bytes, password: str) -> bool:
+    """ checks if password is encrypted
+
+    Args:
+        hashed_pssw (bytes): hashed password
+        password (str): password
+
+    Returns:
+        bool: true or false
+    """
+    return bcrypt.checkpw(password.encode(), hashed_pssw)
