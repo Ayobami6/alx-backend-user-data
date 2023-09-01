9082a2b79c8e8a1751a96a8faa5358c7f2411217
Ayobami <ayobamidele006@gmail.com>
2023-09-01 06:39:49 +0100

    remove type annotation in the instance method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -36,7 +36,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields) -> None:
         """ Instance method
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
