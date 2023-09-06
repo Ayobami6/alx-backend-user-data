124db6f2cb4d345fe4b035bca78520c23332916b
Ayobami <ayobamidele006@gmail.com>
2023-09-06 23:33:19 +0100

    implement unauthorized endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ Unathorized endpoint
+    """
+    abort(401, description="Unauthorized")
