c11953c774fc42540abaf28f2e2b4aea9ff9f1d2
Ayobami <ayobamidele006@gmail.com>
2023-09-13 15:37:18 +0100

    implemet get user by session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,6 +72,20 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ get user from session id
+
+        Args:
+            session_id (str): user session id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
 
 def _generate_uuid() -> str:
     """ Generate unique identifier
